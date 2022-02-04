db349a08a8d36bc21d5a485a564024005ff26b17
jpswt <joel_swt@yahoo.com>
2022-02-04 15:23:35 -0600

    fix mobile nav button directory in scripts.js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,19 +1,19 @@
-window.onload = function(){
+window.onload = function () {
 	var getNavi = document.getElementById('nav-dropdown');
 
 	var mobile = document.createElement("span");
-	mobile.setAttribute("id","mobile-navigation");
-	getNavi.parentNode.insertBefore(mobile,getNavi);
+	mobile.setAttribute("id", "mobile-navigation");
+	getNavi.parentNode.insertBefore(mobile, getNavi);
 
-	document.getElementById('mobile-navigation').onclick = function(){
+	document.getElementById('mobile-navigation').onclick = function () {
 		var x = getNavi.getAttribute('style');
-		if(x){
+		if (x) {
 			getNavi.removeAttribute('style');
-			document.getElementById('mobile-navigation').style.backgroundImage='url(/images/mobile-menu.png)';
+			document.getElementById('mobile-navigation').style.backgroundImage = 'url(../images/mobile-menu.png)';
 		} else {
-  getNavi.style.display='block';
-  document.getElementById('mobile-navigation').style.backgroundImage='url(images/mobile-close.png)';
-}
-};
+			getNavi.style.display = 'block';
+			document.getElementById('mobile-navigation').style.backgroundImage = 'url(../images/mobile-close.png)';
+		}
+	};
 }
 
