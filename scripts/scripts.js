cf88fff56c43e04af7b4aa58dec2a44c1f10fce1
jpswt <joel_swt@yahoo.com>
2022-02-07 18:03:31 -0600

    working contact page with results

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -18,3 +18,10 @@ window.onload = function () {
 	};
 }
 
+const formResults = document.getElementById("results")
+
+new URLSearchParams(window.location.search).forEach((value, name) => {
+	formResults.append(`${name} : ${value}`)
+	formResults.append(document.createElement("br"))
+})
+
