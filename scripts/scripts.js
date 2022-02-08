eb096be1f604c5eb21f266bb221c7034843df11c
jpswt <joel_swt@yahoo.com>
2022-02-07 18:17:42 -0600

    fixed script to reset form on resubmission

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -23,5 +23,8 @@ const formResults = document.getElementById("results")
 new URLSearchParams(window.location.search).forEach((value, name) => {
 	formResults.append(`${name} : ${value}`)
 	formResults.append(document.createElement("br"))
+
 })
+
+document.getElementById("FORM").reset()
 
